78b60cc72ce0fe611e351b6763d5bbfedbc5d154
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2019-10-22 13:43:02 +0200

    .

--- js/comments.js
+++ js/comments.js
@@ -1,5 +1,14 @@
 // "id":"1","name":"Emile Kuhn V","comment":"input Wooden override","date":"2019-07-27T06:22:02.089Z"
 
+class Comment{
+    constructor(name,comment,date){
+        //this.id = 256756;
+        this.name = name; 
+        this.comment = comment; 
+        this.date = date; 
+    }
+}
+
 let commentsAddBtn = document.getElementsByClassName("comment-add-btn")[0].addEventListener("click",createComments);
 let commentContainer = document.getElementsByClassName("comment-inner-container")[0];
 
@@ -23,7 +32,8 @@ function createComments(){
         commentSpan.appendChild(commentP);
         commentContainer.appendChild(commentSpan);
         commentInput.value = "";
-        // let values = mockup.getRandom("comment",5);
+        let newComment = new Comment("Andreas","Ringde ICA","1970-01-01");
+        // let postComment = mockup.post("comment");
         // console.log(values);
     }
 }
