f4c41460c5522651d6b982f50da2ad4b2cfae03d
Andreas <Andreas.Gunnahr@hotmail.com>
2019-10-23 10:34:00 +0200

    Added so the user can add comment with only hitting enter instead of always using the add button

--- js/comments.js
+++ js/comments.js
@@ -9,24 +9,31 @@ class Comment{
     }
 }
 
-let commentsAddBtn = document.getElementsByClassName("comment-add-btn")[0].addEventListener("click",createComments);
+let commentInput = document.getElementsByClassName("comment-input")[0];
+let commentsAddBtn = document.getElementsByClassName("comment-add-btn")[0];
 let commentContainer = document.getElementsByClassName("comment-inner-container")[0];
 
+/* Adding eventListener to our add comment button and if the user hit enter to send the comment. */
+commentsAddBtn.addEventListener("click",createComments);
+commentInput.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        commentsAddBtn.click();
+    }
+});
 
+/* Function that creates our comments and appending it to the comments screen */
 function createComments(){
-    let commentInput = document.getElementsByClassName("comment-input")[0];
     if(commentInput.value != ""){
         let date = new Date();
         let commentSpan = document.createElement("SPAN");
         let commentP = document.createElement("P");
-        commentSpan.classList.add("comment-span");
-        commentP.classList.add("comment-p");
         let fullDate = date.getUTCFullYear() + "-" + date.getUTCDate() + "-" + date.getUTCMonth();
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
         let fullTime = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
-        // + ':' + seconds.toString().padStart(2, '0');
+        commentSpan.classList.add("comment-span");
+        commentP.classList.add("comment-p");
         commentSpan.innerHTML = fullDate + " " + fullTime + ":";
         commentP.innerHTML = commentInput.value;
         commentSpan.appendChild(commentP);
