6621dbf2d1c2dee5fb4f29bcf3e2099e486ef81f
Ella Schwarz <ellaschwarzt@gmail.com>
2019-10-24 12:58:21 +0200

    Generates 5 random deals from mockupapi

--- Deals/js/deals.js
+++ Deals/js/deals.js
@@ -1,10 +1,6 @@
 class Deals {
-    constructor(company, contact, product, price) {
-        this.deal;
-        this.company = company;
-        this.contact = contact;
-        this.product = product;
-        this.price = price;
+    constructor() {
+        this.companies;
 
     }
 
@@ -16,43 +12,85 @@ async getDeals() {
         return customer.companyName;
     })
 
-    console.log(companies);
+    // console.log(companies);
 
     deals.forEach((deal, i) => {
-        let orderDeal = new Deals(deal.company, deal.contact, deal.product, deal.price);
+        let orderDeal = new Order (deal.company, deal.fullName, deal.productName, deal.price);
         orderDeal.createDeal();
     })
 }
 
+addNewDeal () {
+
+}
+
+}
+
+class Order {
+    constructor(company, contact, product, price) {
+        this.deal;
+        this.company = company;
+        this.contact = contact;
+        this.product = product;
+        this.price = price;
+    }
+
+    generateRandom() {
+
+    }
+
 createDeal() {
-    
+
     var table = document.getElementsByClassName("deals_table");
-    console.log(table);
+  
     
     var row = document.createElement("tr");
-    console.log(row);
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");    
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");  
 
     td1.innerHTML = document.getElementById("company").value;
     td2.innerHTML  = document.getElementById("contact").value;
     td3.innerHTML  = document.getElementById("product").value;
     td4.innerHTML  = document.getElementById("price").value;
 
+    td1.innerHTML = this.company;
+    td2.innerHTML  = this.contact;
+    td3.innerHTML  = this.product;
+    td4.innerHTML  = this.price;
+
     row.appendChild(td1);
     row.appendChild(td2);
     row.appendChild(td3);
     row.appendChild(td4);
 
+    // let td5 = document.createElement("td");
+    // let td6 = document.createElement("td");
+    // let td7 = document.createElement("td");
+    // let td8 = document.createElement("td");  
+
+
+    
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+
+    // row.appendChild(this.company, this.contact, this.product, this.price);
+
     table[0].children[0].appendChild(row);
+
+    document.getElementsByClassName("deals_table")[0].style.display = "block";
+    document.getElementsByClassName("addDeal_container")[0].style.display = "none";
 };
 }
 
 
 let allDeals = new Deals();
-// let deal = null;
+let deal = null;
+let allOrders = new Order ();
 
 function openPlus() {
     var add = document.getElementsByClassName("addDeal_container")[0];
@@ -65,16 +103,7 @@ function openPlus() {
     }
 };
 
-// function closePlus () {
-//         var add = document.getElementsByClassName("addDeal_container")[0];
-//         if (add.style.display === "block") {
-//             add.style.display = "none";
-//             document.getElementsByClassName("deals_table")[0].style.display = "block";
-//         } else {
-//             add.style.display = "block";
-//             document.getElementsByClassName("deals_table")[0].style.display = "none";
-//         }
-// };
 
-// document.getElementsByClassName("grid")[0].style.display = "block";
-// document.getElementById("header").style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+    allDeals.getDeals();
+});
